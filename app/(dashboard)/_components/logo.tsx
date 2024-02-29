cd0844cf91828f53ab333594d7d420a3df2399ea
Ankit Gupta <g.ankit2001@gmail.com>
2024-03-01 00:32:15 +0530

    Added Login and Signup pages

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export const Logo = () => {
+    return (
+        <Image 
+        height={113}
+        width={113}
+        alt='logo'
+        src='/UEM-Symbol.png'
+        />
+    )
+}

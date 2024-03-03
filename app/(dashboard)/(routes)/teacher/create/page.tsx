7a3ee95b5780e716913029f423c1583909b5586d
Ankit Gupta <g.ankit2001@gmail.com>
2024-03-03 20:35:35 +0530

    Added Course Upload Page and Backend API

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -40,7 +40,8 @@ const CreatePage = () => {
     const onSubmit = async(values: z.infer<typeof formSchema>) => {
         try {
             const response=await axios.post('/api/courses', values);
-            router.push(`/teacher/courses/${response.data.id}`)
+            router.push(`/teacher/courses/${response.data.id}`);
+            toast.success('Course created');
         } catch (error) {   
             toast.error('Something went wrong');
         }

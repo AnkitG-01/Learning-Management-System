abd01881dc4a56f654f84e632a93042ad74ee4bc
Ankit Gupta <g.ankit2001@gmail.com>
2024-03-09 10:49:36 +0530

    Added Category Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseIdPage = async ({
     params
@@ -21,6 +22,12 @@ const CourseIdPage = async ({
     const course = await db.course.findUnique({
         where: {
             id: params.courseId
+        }
+    });
+
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: "asc"
         }
     });
 
@@ -66,6 +73,14 @@ const CourseIdPage = async ({
                     <ImageForm
                         initialData = { course }
                         courseId = { course.id }    
+                    />
+                    <CategoryForm 
+                    initialData = { course }
+                    courseId = { course.id }
+                    options={categories.map((category) => ({
+                        label: category.name,
+                        value: category.id
+                    }))}
                     />
                 </div>
             </div>
